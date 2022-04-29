2a0248fe4742d93f80ba2cd3ca83924c7c4993da
Esther Annorzie <estherannorzie@gmail.com>
2022-04-29 02:10:50 -0400

    changed app init

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,13 @@ def create_app(test_config=None):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
 
+    from app.models.book import Book
+    
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
     from .routes import books_bp
     app.register_blueprint(books_bp)
-
-    from app.models.book import Book
 
     return app
