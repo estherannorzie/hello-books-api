ff4f5e9aa7481ebbb4ac9531f1417fb81e519c8f
Esther Annorzie <estherannorzie@gmail.com>
2022-04-30 14:59:40 -0400

    finished creating one book

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,23 @@
-from flask import Blueprint, abort, make_response
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
 # creating endpoint
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+# decorating the endpoint
+@books_bp.route("", methods=["POST"])
+def create_book():
+    # "Pythonifies" the JSON HTTP request body by converting it to a Python dictionary.
+
+    request_body = request.get_json()
+    # kwargs match model attributes and access request body
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+    
+    # db.session collects changes that need to be made for the database, we add then commit to save changes
+    db.session.add(new_book)
+    db.session.commit()
+
+    # 200 is the default of
+    return make_response(f"Book {new_book.title} successfully created", 201)
