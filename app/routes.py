4acdbf09b0bae6897ce4ba7f7c405845b8cce5cf
Esther Annorzie <estherannorzie@gmail.com>
2022-05-01 15:24:08 -0400

    refactored get and post methods

--- app/routes.py
+++ app/routes.py
@@ -6,28 +6,31 @@ from flask import Blueprint, jsonify, make_response, request
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 # decorating the endpoint
-@books_bp.route("", methods=["POST", "GET"])
-def handle_books():
-    # "Pythonifies" the JSON HTTP request body by converting it to a Python dictionary.
-    request_body = request.get_json()
-
-    if request.method == "POST"
-        # kwargs match model attributes and access request body
-        new_book = Book(title=request_body["title"],
-                        description=request_body["description"])
-        
-        # db.session collects changes that need to be made for the database, we add then commit to save changes
-        db.session.add(new_book)
-        db.session.commit()
-        
-        return make_response(f"Book {new_book.title} successfully created", 201)
-
-    elif request.method == "GET":
-        # turns all instance of book into a list
+# there is a comma near the methods arg since it's a tuple with one element, 
+# else python thinks it's a string
+@books_bp.route("", methods=("GET",))
+def get_all_books ():
+ # turns all instance of book into a list
         books = Book.query.all()
-        books_response = [dict(title=book.title, 
+        books_response = [dict(id=book.id,
+                               title=book.title, 
                                description=book.description) for book in books]
         
         return jsonify(books_response)
 
-    
+
+@books_bp.route("", methods=("POST",))
+def create_book():
+    # HTTP request body converted to a Python dictionary.
+    request_body = request.get_json()
+
+    # kwargs match model attributes and access request body
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+        
+    # db.session collects changes that need to be made for the database, 
+    # we add then commit to save changes
+    db.session.add(new_book)
+    db.session.commit()
+        
+    return make_response(f"Book {new_book.title} successfully created", 201)
